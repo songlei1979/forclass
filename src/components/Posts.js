a59a7698ee0f7fec5ffa57b606624e0bb3082082
Lei <gabriel_sl19798@hotmail.com>
2024-10-30 20:31:53 +1300

    delete

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -8,18 +8,38 @@ function Posts(props) {
     const [loginStatus, setLoginStatus] = useState(false)
     const [posts, setPosts] = useState([])
     const [error, setError] = useState("")
+    const [author, setAuthor] = useState(0)
 
     useEffect(() => {
         if (localStorage.getItem('token') !== null) {
             setLoginStatus(true);
             setToken(localStorage.getItem('token'));
+            let config = {
+                method: 'get',
+                maxBodyLength: Infinity,
+                url: baseUrl + 'api/get_user_id/',
+                headers: {
+                    'Authorization': 'token ' + localStorage.getItem('token')
+                }
+            };
+
+            axios.request(config)
+                .then((response) => {
+                    console.log(JSON.stringify(response.data));
+                    setAuthor(response.data);
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
         }
 
         let config = {
             method: 'get',
             maxBodyLength: Infinity,
             url: baseUrl + 'api/posts/',
-            headers: {}
+            headers: {
+
+            }
         };
 
         axios.request(config)
@@ -34,7 +54,26 @@ function Posts(props) {
                 setError(error)
             });
 
-    }, []);
+    }, [posts, loginStatus]);
+
+    function deletePost(id) {
+        let config = {
+            method: 'delete',
+            maxBodyLength: Infinity,
+            url: baseUrl + 'api/posts/' + id + '/',
+            headers: {
+                'Authorization': 'token ' + token
+            }
+        };
+
+        axios.request(config)
+            .then((response) => {
+                console.log(JSON.stringify(response.data));
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
 
     return (
         <div>
@@ -43,9 +82,15 @@ function Posts(props) {
 
             {posts.map((post) => (
                 <div key={post.id}>
-                    <Link to={`/post_detail/`} state={{ id: post.id }}>
+                    <Link to={`/post_detail/`} state={{id: post.id}}>
                         {post.title}
                     </Link>
+                    {post.author === author ?
+                        <div>
+                            <button>Update</button>
+                            <button onClick={deletePost.bind(this, post.id)}>Delete</button>
+                        </div>
+                        : null}
                 </div>
             ))}
         </div>
